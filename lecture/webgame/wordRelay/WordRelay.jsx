42cd9684814636663ac30699b5ab37f56760e9e7
hcs1105 <hcs1105@gmail.com>
2022-04-24 14:15:50 +0900

    14강 수업 내용 정리

---
+++ lecture/webgame/wordRelay/WordRelay.jsx
@@ -0,0 +1,15 @@
+/* npm으로 설치한 React을 불러옴 */
+const React = require('react');
+const { Component } = React;
+
+class WordRelay extends Component{
+  state = {
+    
+  };
+
+  render() {
+
+  }
+}
+
+module.exports = WordRelay;
--- lecture/webgame/wordRelay/webpack.config.js
+++ lecture/webgame/wordRelay/webpack.config.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const path = require('path');
+
+module.exports = {
+  name : 'word-relay-setting', // 웹팩 설정 환경 이름
+  mode : 'development', // 개발용이며 실 서비스로 배포할 때는 'production'으로 변경
+  devtool : 'eval',
+  resolve : {
+    extension : ['.js', 'jsx']
+  },
+
+  entry : {
+    app : ['Client']
+  }, // 입력
+  output : {
+    path : path.join(__dirname, 'dist'), // __dirname : 현재 폴더 경로(F:\자바스크립트 스터디\React\lecture\webgame\wordRelay\dist)
+    filename : 'app.js' // 출력할 파일 이름 지정
+  }, // 출력
+};

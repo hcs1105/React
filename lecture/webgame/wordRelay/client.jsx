b43be03a4db32c3fb61f288d1b59c73e7426f08b
hcs1105 <hcs1105@gmail.com>
2022-05-10 21:43:46 +0900

    15강 수업 내용 정리 중

--- lecture/webgame/wordRelay/client.jsx
+++ lecture/webgame/wordRelay/client.jsx
@@ -1,6 +1,6 @@
 /* npm으로 설치한 React와 React-DOM을 불러옴 */
 const React = require('react');
 const ReactDom = require('react-dom');
-const WordRelay = require('WordRelay');
+const WordRelay = require('./WordRelay');
 
 ReactDom.render(<WordRelay />, document.querySelector('#root'));
--- lecture/webgame/wordRelay/webpack.config.js
+++ lecture/webgame/wordRelay/webpack.config.js
@@ -5,11 +5,10 @@ module.exports = {
   mode : 'development', // 개발용이며 실 서비스로 배포할 때는 'production'으로 변경
   devtool : 'eval',
   resolve : {
-    extension : ['.js', 'jsx']
+    extensions : ['.js', '.jsx']
   },
-
   entry : {
-    app : ['Client']
+    app : ['./client'],
   }, // 입력
   output : {
     path : path.join(__dirname, 'dist'), // __dirname : 현재 폴더 경로(F:\자바스크립트 스터디\React\lecture\webgame\wordRelay\dist)

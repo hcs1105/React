8d431bcc274392fb6afac00d38525ee0b72bf7f8
hcs1105 <hcs1105@gmail.com>
2022-05-12 21:49:53 +0900

    15강 수업 내용 정리

--- lecture/webgame/wordRelay/webpack.config.js
+++ lecture/webgame/wordRelay/webpack.config.js
@@ -10,6 +10,16 @@ module.exports = {
   entry : {
     app : ['./client'],
   }, // 입력
+  module : {
+    rules : [{
+      test : /\.jsx?/, // js 파일과 jsx 파일에 rule를 적용함.
+      loader : 'babel-loader',
+      options : {
+        presets : ['@babel/preset-env', '@babel/preset-react'],
+        plugins : ['@babel/plugin-proposal-class-properties'],
+      },
+    }],
+  },
   output : {
     path : path.join(__dirname, 'dist'), // __dirname : 현재 폴더 경로(F:\자바스크립트 스터디\React\lecture\webgame\wordRelay\dist)
     filename : 'app.js' // 출력할 파일 이름 지정

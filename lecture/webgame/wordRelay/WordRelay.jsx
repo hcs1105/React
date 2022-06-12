d8e020af95eae6f6f00abef19a9027efdd208100
hcs1105 <hcs1105@gmail.com>
2022-06-12 16:12:57 +0900

    18강 수업 내용 정리 및 클래스형 컴포넌트로 끝말 잇기 구현

--- lecture/webgame/wordRelay/WordRelay.jsx
+++ lecture/webgame/wordRelay/WordRelay.jsx
@@ -1,14 +1,58 @@
-/* npm으로 설치한 React을 불러옴 */
+/** 
+ * npm으로 설치한 React을 불러옴.
+ */
 const React = require('react');
 const { Component } = React;
 
 class WordRelay extends Component{
   state = {
-    text : 'Hello! Webpack',
+    word : '한창수',
+    value : '',
+    result : '',
   };
 
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if(this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result : '딩동댕',
+        word : this.state.value,
+        value : ''
+      });
+      this.input.focus();
+    } else {
+      this.setState({
+        result : '땡',
+        value : '',
+      });
+      this.input.focus();
+    }
+  };
+
+  input;
+
+  onRefInput = (c) => {
+    this.input = c;
+  };
+
+  onChangeInput = (e) => {
+    this.setState({
+      value : e.currentTarget.value
+    });
+  }
+
   render() {
-    return <h1>{this.state.text}</h1>;
+    return ( 
+      <>
+        <div>{this.state.word}</div>
+        <form method="post" onSubmit={this.onSubmitForm}>
+          {/* <input type="text" ref={this.onRefInput} defaultValue={this.state.value} /> */}
+          <input type="text" ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+          <button type="submit">입력</button>
+          <div>{this.state.result}</div>
+        </form>
+      </>
+    );
   }
 }
 

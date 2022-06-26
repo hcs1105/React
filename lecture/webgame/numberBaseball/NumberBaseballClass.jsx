ca3142f4a21d3765075436f690648585fb76651b
hcs1105 <hcs1105@gmail.com>
2022-06-26 19:57:19 +0900

    26강 수업 내용 정리 - 숫자 야구 만들기 진행 중

--- lecture/webgame/numberBaseball/NumberBaseballClass.jsx
+++ lecture/webgame/numberBaseball/NumberBaseballClass.jsx
@@ -2,24 +2,46 @@ import React, { Component } from 'react';
 import Tries from './Tries';
 
 function getNumbers(){ // 숫자 4개를 겹치지 않고 랜덤하게 뽑는 함수
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
 
+  for(let i = 0; i < 4; i++) {
+    const chosen = candidate.splice(Math.floor(Math.random()*(9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
 }
 
 class NumberBaseball extends Component {
   state = {
     result : '',
     value : '',
-    answer : getNumbers(),
+    answer : getNumbers(), // ex) [1, 3, 5, 7]
     tries : []
   };
 
   onSubmitForm = (e) => {
     e.preventDefault();
+    if(this.state.value === this.state.answer.join('')) { // 입력한 숫자가 정답인 경우
+      this.setState({
+        result : '홈런',
+        tries : [...this.state.tries, { try : this.setState.value, result : '홈런'}]
+      });
+    } else {
+      const answerArray = this.state.value.split('').map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+
+      if(this.state.tries.length >= 9) {
+
+      }
+    }
     console.log(this.state.value);
   };
 
   onChangeInput = (e) => {
     console.log(this);
+    console.log(this.state.answer);
     this.setState({
       value : e.target.value
     });

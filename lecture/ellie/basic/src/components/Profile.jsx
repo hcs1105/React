b83b3c52889e6027a07b706ff4c32e859ee72930
hcs1105 <hcs1105@gmail.com>
2023-03-06 21:02:06 +0900

    Profile 컴포넌트 UI 완성

--- lecture/ellie/basic/src/components/Profile.jsx
+++ lecture/ellie/basic/src/components/Profile.jsx
@@ -1,7 +1,9 @@
 export default function Profile(){
   return (
-    <>
-      <h1>Profile</h1>
-    </>
+    <div className="profile-area">
+      <img src="https://avatars.githubusercontent.com/u/8123489?v=4" className="profile-img" alt="한창수" />
+      <h1>한창수</h1>
+      <p>프론트엔드 개발자</p>
+    </div>
   );
 }

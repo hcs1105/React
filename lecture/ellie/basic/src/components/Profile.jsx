0b7e9ab4342b5887aeed3623230e5173208b14b1
hcs1105 <hcs1105@gmail.com>
2023-03-06 21:26:56 +0900

    props와 관련된 소스 추가

--- lecture/ellie/basic/src/components/Profile.jsx
+++ lecture/ellie/basic/src/components/Profile.jsx
@@ -1,9 +1,21 @@
-export default function Profile(){
+export default function Profile({image, name, job}){
   return (
     <div className="profile-area">
-      <img src="https://avatars.githubusercontent.com/u/8123489?v=4" className="profile-img" alt="한창수" />
-      <h1>한창수</h1>
-      <p>프론트엔드 개발자</p>
+      <img src={image} className="profile-img" alt="한창수" />
+      <h1>{name}</h1>
+      <p>{job}</p>
     </div>
   );
 }
+
+/* 동일한 표현 방식
+export default function Profile(props){
+  return (
+    <div className="profile-area">
+      <img src={props.image} className="profile-img" alt="한창수" />
+      <h1>{props.name}</h1>
+      <p>{props.job}</p>
+    </div>
+  );
+}
+*/

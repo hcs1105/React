4877d6a9dd4df975c407d6f33110305545b807eb
hcs1105 <hcs1105@gmail.com>
2023-03-06 20:37:58 +0900

    Profile 컴포넌트 추가

--- lecture/ellie/basic/src/AppProfile.jsx
+++ lecture/ellie/basic/src/AppProfile.jsx
@@ -1,18 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile';
 
 function AppProfile() {
-  const name = 'hcs1105';
-  const itemList = ['우유', '딸기', '바나나'];
-
   return (
     <>
-      <h1>{`Hello! ${name}`}</h1>
-      <h2>Hi!</h2>
-      <p>{name}</p>
-      <ul>
-        {itemList.map(item => <li>{item}</li>)}
-      </ul>
+      <Profile />
     </>
   );
 }

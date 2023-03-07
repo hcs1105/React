b9111de7ece2265123b20bfd8d1b26830b8056f7
hcs1105 <hcs1105@gmail.com>
2023-03-07 19:18:02 +0900

    useState() 메소드를 이용하여 숫자 카운트 로직 구현

--- lecture/ellie/basic/src/components/Counter.jsx
+++ lecture/ellie/basic/src/components/Counter.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="counter-container">
       <div className="counter-area">
-        <span className="counter-number">0</span>
-        <button className="counter-button">Add +</button>
+        <span className="counter-number">{count}</span>
+        <button className="counter-button" onClick={() => {
+          setCount(count + 1);
+        }}>Add +</button>
       </div>
     </div>
   );

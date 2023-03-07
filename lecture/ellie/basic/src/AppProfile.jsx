9baefd555dc2bbadbf6083d2ade0c9b95f9d7148
hcs1105 <hcs1105@gmail.com>
2023-03-07 16:43:11 +0900

    Avartar 컴포넌트 수정

--- lecture/ellie/basic/src/AppProfile.jsx
+++ lecture/ellie/basic/src/AppProfile.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Avartar from './components/Avartar';
 import Profile from './components/Profile';
 
 function AppProfile() {
   return (
     <>
+      <Avartar image='https://avatars.githubusercontent.com/u/8123489?v=4' newIcon={true} />
       <Profile image='https://avatars.githubusercontent.com/u/8123489?v=4' name='한창수' job='프론트엔드 개발자' newIcon={true} />
       <Profile image='https://avatars.githubusercontent.com/u/8123489?v=4' name='이창수' job='백엔드 개발자' />
       <Profile image='https://avatars.githubusercontent.com/u/8123489?v=4' name='삼창수' job='프론트엔드 개발자' />
--- lecture/ellie/basic/src/components/Avartar.jsx
+++ lecture/ellie/basic/src/components/Avartar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Avartar({image, newIcon}) {
   return (
-    <div>
+    <div className="avartar-area">
       <img src={image} className="profile-img" alt="한창수" />
       {newIcon && <span className="new-icon">New</span>}
     </div>

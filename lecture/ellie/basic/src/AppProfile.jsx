44144014daf4df5666e14a7cdf3460415b327e2e
hcs1105 <hcs1105@gmail.com>
2023-03-07 17:50:24 +0900

    이벤트를 작성하는 방법 정리

--- lecture/ellie/basic/src/AppProfile.jsx
+++ lecture/ellie/basic/src/AppProfile.jsx
@@ -3,8 +3,13 @@ import Avartar from './components/Avartar';
 import Profile from './components/Profile';
 
 function AppProfile() {
+  const handleClick = (e) => {
+    console.log(e);
+    alert('버튼이 클릭됨');
+  }
   return (
     <>
+      <button type='button' onClick={handleClick}>버튼</button>
       <Avartar image='https://avatars.githubusercontent.com/u/8123489?v=4' newIcon={true} />
       <Profile image='https://avatars.githubusercontent.com/u/8123489?v=4' name='한창수' job='프론트엔드 개발자' newIcon={true} />
       <Profile image='https://avatars.githubusercontent.com/u/8123489?v=4' name='이창수' job='백엔드 개발자' />

152ee61eee091a8d1e7a3195dad72550fab746d7
hcs1105 <hcs1105@gmail.com>
2023-03-07 18:40:17 +0900

    Counter 컴포넌트 UI 완료

---
+++ lecture/ellie/basic/src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Counter() {
+  return (
+    <div className="counter-container">
+      <div className="counter-area">
+        <span className="counter-number">0</span>
+        <button className="counter-button">Add +</button>
+      </div>
+    </div>
+  );
+} 

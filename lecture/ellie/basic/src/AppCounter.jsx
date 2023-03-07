53a3bb8313c71888f8f8e2062197beacaef0e031
hcs1105 <hcs1105@gmail.com>
2023-03-07 20:50:02 +0900

    개별 컴포넌트와 전체 컴포넌트의 카운터를 구하는 로직 구현

--- lecture/ellie/basic/src/AppCounter.jsx
+++ lecture/ellie/basic/src/AppCounter.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
 export default function AppCounter() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(prev => prev + 1);
+  }
+
   return (
-    <div>
-      <Counter />
-    </div>
+    <>
+      <div className="counter-banner">
+        Total Count: {count} {count > 10 ? '🔥' : '❄️'}
+      </div>
+      <Counter total={count} onClick={handleClick} />
+      <Counter total={count} onClick={handleClick} />
+    </>
   );
 }

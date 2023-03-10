843d9226c3e5f96e202ddfda4f691539ac023daf
hcs1105 <hcs1105@gmail.com>
2023-03-10 14:33:42 +0900

    마우스 따라가기 로직에 적용된 변수 수정

--- lecture/ellie/basic/src/AppXY.jsx
+++ lecture/ellie/basic/src/AppXY.jsx
@@ -2,24 +2,17 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  /*
-  const [coordX, setCoordX] = useState(0);
-  const [coordY, setCoordY] = useState(0);
-  */
-  const [position, setPosition] = useState({ x:0, y:0 });
+  const [x, setCoordX] = useState(0);
+  const [y, setCoordY] = useState(0);
   const onPoinerMove = (e) => {
     console.log(e.clientX, e.clientY);
-    /*
     setCoordX(e.clientX);
     setCoordY(e.clientY);
-    */
-   setPosition({ coordX: e.clientX, coordY:e.clientY})
   }
 
   return (
     <div className="container" onPointerMove={onPoinerMove}>
-      {/* <div className="pointer" style={{transform: `translate(${coordX}px, ${coordY}px)`}}></div> */}
-      <div className="pointer" style={{transform: `translate(${position.coordX}px, ${position.coordY}px)`}}></div>
+      <div className="pointer" style={{transform: `translate(${x}px, ${y}px)`}}></div>
     </div>
   );
 }

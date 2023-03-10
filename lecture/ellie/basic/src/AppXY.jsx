3d3fb9e3c4635ebb5d9821ab559c8c69bb46ad27
hcs1105 <hcs1105@gmail.com>
2023-03-10 12:38:16 +0900

    마우스를 움직일 때마다 원이 마우스 위치에 이동하도록 기능 구현

--- lecture/ellie/basic/src/AppXY.jsx
+++ lecture/ellie/basic/src/AppXY.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
+  const [coordX, setCoordX] = useState(0);
+  const [coordY, setCoordY] = useState(0);
+  const onPoinerMove = (e) => {
+    console.log(e.clientX, e.clientY);
+    setCoordX(e.clientX);
+    setCoordY(e.clientY);
+  }
 
   return (
-    <div className="container">
-      <div class="pointer"></div>
+    <div className="container" onPointerMove={onPoinerMove}>
+      <div className="pointer" style={{transform: `translate(${coordX}px, ${coordY}px)`}}></div>
     </div>
   );
 }

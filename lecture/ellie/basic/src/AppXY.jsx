8f932c6afec81c841203aad335d533f5cca3bd5c
hcs1105 <hcs1105@gmail.com>
2023-03-10 14:42:06 +0900

    마우스 따라가기 로직 리팩토링

--- lecture/ellie/basic/src/AppXY.jsx
+++ lecture/ellie/basic/src/AppXY.jsx
@@ -2,17 +2,24 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
+  /*
   const [x, setCoordX] = useState(0);
   const [y, setCoordY] = useState(0);
+  */
+  const [position, setPosition] = useState({x:0, y:0});
   const onPoinerMove = (e) => {
     console.log(e.clientX, e.clientY);
+    /*
     setCoordX(e.clientX);
     setCoordY(e.clientY);
+    */
+   setPosition({x:e.clientX, y:e.clientY});
   }
 
   return (
     <div className="container" onPointerMove={onPoinerMove}>
-      <div className="pointer" style={{transform: `translate(${x}px, ${y}px)`}}></div>
+      {/* <div className="pointer" style={{transform: `translate(${x}px, ${y}px)`}}></div> */}
+      <div className="pointer" style={{transform: `translate(${position.x}px, ${position.y}px)`}}></div>
     </div>
   );
 }

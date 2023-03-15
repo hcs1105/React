ab93f6dca6ca31841401b672ff503093ba997395
hcs1105 <hcs1105@gmail.com>
2023-03-15 18:29:31 +0900

    immer 로직 구현 완료

--- lecture/ellie/basic/src/AppMentorsImmer.jsx
+++ lecture/ellie/basic/src/AppMentorsImmer.jsx
@@ -6,13 +6,11 @@ export default function AppMentorsImmer() {
     name: '창수',
     title: '주니어 개발자',
     mentors: [{
-      id : 1,
       name : 'James',
       title : '시니어 개발자',
       
     }, 
     {
-      id : 2,
       name : 'Steve Jobs',
       title : '시니어 개발자',
     }]
@@ -22,35 +20,31 @@ export default function AppMentorsImmer() {
   const changeMentorName = () => {
     const prev = prompt('누구의 이름을 바꾸고 싶은가요?');
     const current = prompt('이름을 무엇으로 바꾸고 싶은가요?');
-    /*
-    setPerson(person => ({
-      ...person, 
-      mentors: person.mentors.map(mentor => {
-        if(mentor.name === prev) {
-          return {...mentor, name: current};
-        } 
-        return mentor;
-      })
-    }));*/
+
+    // updatePerson(person => person.name = '새로운 이름!');
+    updatePerson(person => {
+      const mentor = person.mentors.find(m => m.name === prev);
+      mentor.name = current;
+    });
   };
 
   const addMentorName = () => {
     const name = prompt('멘토의 이름은?');
     const title = prompt('멘토의 직함은?');
 
-    setPerson(person => ({
-      ...person, 
-      mentors : [...person.mentors, {name, title}]
-    }));
+    updatePerson(person => {
+      person.mentors.push({name, title});
+    });
   };
 
   const deleteMentorName = () => {
     const name = prompt('누구를 삭제하고 싶은가요?');
+    
+    updatePerson(person => {
+      const index = person.mentors.findIndex(m => m.name === name);
 
-    setPerson(person => ({
-      ...person, 
-      mentors: person.mentors.filter(mentor => mentor.name != name)
-    }));
+      person.mentors.splice(index, 1);
+    });
   };
 
   return (
@@ -59,7 +53,7 @@ export default function AppMentorsImmer() {
       <p>{person.name}의 멘토는</p>
       <ul>
         {person.mentors.map(mentor => (
-          <li key={mentor.id}>{mentor.name} {mentor.title}</li>
+          <li key={mentor.name}>{mentor.name} {mentor.title}</li>
         ))}
       </ul>
       <button type="button" onClick={changeMentorName}>멘토 이름 바꾸기</button>

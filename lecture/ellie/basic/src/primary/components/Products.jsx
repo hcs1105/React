c2c0972c5156a8455b611f5fddbe6662b265009b
hcs1105 <hcs1105@gmail.com>
2023-03-16 17:17:32 +0900

    로딩과 에러 상태 구현하기

--- lecture/ellie/basic/src/primary/components/Products.jsx
+++ lecture/ellie/basic/src/primary/components/Products.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
   const handleChange = () => {
-    setChecked((prev) => !prev);
+    setChecked(prev => !prev);
   }
 
   /**
@@ -24,17 +26,29 @@ export default function Products() {
 
   /* cleanUp() 함수 호출 → setProducts(data) 메소드 실행 → useState([]) 업데이트 */
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
     fetch(`data/${checked ? 'sales_' : ''}products.json`) 
     .then(res => res.json())
     .then(data => {
       console.log('따뜻한 데이터를 네트워크에서 받아 옴');
       setProducts(data);
-    });
+    })
+    .catch(e => setError('에러가 발생했음'))
+    .finally(() => setLoading(false));
     
     return () => { 
       console.log('깨끗하게 청소하는 일들을 합니다.');
     }
   }, [checked]);
+
+  if(loading) {
+    return <p>로딩 중</p>;
+  }
+  
+  if(error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <>

de46516fdea9dc27cd12dfe3808a15b6fbf6497f
hcs1105 <hcs1105@gmail.com>
2023-03-17 13:29:53 +0900

    PostCSS 작성하기

---
+++ lecture/ellie/basic/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button1 from './components/Button1';
+import Button2 from './components/Button2';
+import './primary/App.css';
+
+export default function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />  
+    </>
+  );
+}
---
+++ lecture/ellie/basic/src/components/Button1.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from'./button1.module.css';
+
+export default function Button1() {
+  return <button type="button" className={styles.button}>버튼 1</button>;
+}
